cc84e2a96bec1ae29bbbc1c7398f2b32405e9a52
posfiapteam35@gmail.com <posfiapteam35@gmail.com>
2026-02-04 23:53:38 -0300

    commit inicial

---
+++ src/main/java/br/com/gerenciamento/FiapHackatonGerenciamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.gerenciamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapHackatonGerenciamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiapHackatonGerenciamentoApplication.class, args);
+	}
+
+}
